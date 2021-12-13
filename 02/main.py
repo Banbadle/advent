4a2f30e56792b9f603088d3131a813d6e9916c75
Banbadle <archiealmond@hotmail.co.uk>
2021-12-13 04:58:26 +0000

    Update Day 2

--- 02/main.py
+++ 02/main.py
@@ -1,27 +1,26 @@
+with open("input.txt", "r+") as f:
+    data = [(direction, int(num))for direction, num in [line.split() for line in f]]
+
 dir_map = {"forward": (0,1), "up": (1,-1), "down": (1,1)}
 
-with open("input.txt", "r+") as f:
-    total = [0,0]
-    for line in f:
-        direction, num = line.split()
-        ind, mult = dir_map[direction]
-        
-        total[ind] += int(mult) * int(num)
-        
-    print(total[0] * total[1])
+#Part 1
+total = [0,0]
+for direction, num in data:
+    dir_ind, mult = dir_map[direction]
+    total[dir_ind] += mult * num
     
-with open("input.txt", "r+") as f:
-    aim = 0
-    total = [0,0]
-    for line in f:
-        direction, num = line.split()
-        ind, mult = dir_map[direction]
-        
-        if direction != "forward":
-            aim += int(mult) * int(num)
-        
-        else:
-            total[0] += int(mult) * int(num)
-            total[1] += aim * int(num)
-        
-    print(total[0] * total[1])
+print(total[0] * total[1])
+    
+#Part 2
+aim = 0
+depth, distance = (0,0)
+for direction, num in data:
+    _, mult = dir_map[direction]
+    
+    if direction != "forward":
+        aim += mult * num
+    else:
+        distance += mult * num
+        depth += aim * num
+    
+print(distance * depth)
